85c234785a4498f34bac9d95f74f49d6db076da1
cjwd <chinarakinda@gmail.com>
2019-02-11 16:12:09 -0400

    Require example controller

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var exampleController = require('../controllers/exampleController');
 
 /* GET home page. */
 router.get('/', exampleController.displayUsers);
